7096c03715c32f1ac50a42510215b6cbc482b925
erenkavaklioglu <erenkavaklioglu@yahoo.com>
2019-03-08 16:29:29 +0300

    Client side operations are coding

---
+++ TestClient/EmployeeManager.cs
@@ -0,0 +1,23 @@
+﻿#region Signature
+/*
+Author : Eren Kavaklıoğlu
+Year   : 2019
+*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClient
+{
+    /// <summary>
+    /// Employee management class
+    /// </summary>
+    public class EmployeeManager
+    {
+
+    }
+}
